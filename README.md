ec7f8fb951589f85dcdd8c8474615348fdd6cb4b
Jonathon Kelly <jonathon.kelly@insidersbyte.com>
2016-03-07 13:36:37 +0000

    add initial files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# wemo-wrapper
+# Wemo Client Wrapper for Node.js
+
+This is a abstraction built on top of https://github.com/timonreinhard/wemo-client.
---
+++ constants/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+    DEBUG_KEY: 'wemo-wrapper',
+
+    DEVICE_TYPE: {
+        SWITCH: 'Socket',
+        INSIGHT: 'Insight',
+    },
+
+    EVENTS: {
+        BINARY_STATE_EVENT_NAME: 'binaryState',
+        INSIGHT_PARAMS_EVENT_NAME: 'insightParams',
+    },
+
+    DISCOVERY_TIMEOUT: 5000,
+};
